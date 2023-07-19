0105729726728957168f6b33ba1b531cb03d8397
Francis <francisjeevesjulayco@gmail.com>
2023-07-20 02:07:01 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import  Unity, { UnityContext } from "react-unity-webgl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const unityContext = new UnityContext({
+    loaderUrl: "./Game/Build/Game.loader.js",
+    dataUrl:   "./Game/build/Game.data",
+    frameworkUrl: "./Game/build/Game.framework.js",
+    codeUrl: "./Game/build/Game.wasm",
+  });
+    
+    function App() {
+    return(
+        <div className= "App">
+          <Unity 
+           unityContext={unityContext} 
+           style={{
+               width: "600px",
+               height: "400px",
+               border: "2px solid black",
+               background: "green",
+           }}
+         />
+          </div>
+    );
+
 }
 
 export default App;
